2bd9a98b5ddc1a301674b75d2b7f3fc3b30cf9a3
Calcitem <calcitem@outlook.com>
2023-03-07 00:40:27 +0800

    增加 makeXXX 函数

--- chaosclock.cpp
+++ chaosclock.cpp
@@ -147,6 +147,74 @@ std::vector<int8_t> vectorMerge(const std::vector<int8_t> hand,
     return make_free;
 }
 
+void makeFree(size_t p, Pieces &new_pieces, const Position &pos)
+{
+    vector<int8_t> makefree = vectorMerge(new_pieces.hand[p],
+                                          new_pieces.free[p]);
+    for (long x = 0; x < new_pieces.stop[p].size(); x++) {
+        int stopx = new_pieces.stop[p][x];
+        int8_t stop_pos = vectorIndexOf(pos.board, stopx);
+        if (vectorIndexOf(makefree, stop_pos + 1) > -1) {
+            new_pieces.free[p].emplace_back(stopx);
+            new_pieces.stop[p].erase(new_pieces.stop[p].begin() + x);
+            makefree = vectorMerge(new_pieces.hand[p], new_pieces.free[p]);
+            x = -1;
+        }
+    }
+}
+
+void makeStock(size_t p, Pieces &new_pieces, const Position &pos,
+               const std::vector<int8_t> &run_pos_sum)
+{
+    vector<int8_t> stop_delete;
+    for (size_t x = 0; x < new_pieces.stop[p].size(); x++) {
+        int c = new_pieces.stop[p][x];
+        int c_pos = vectorIndexOf(pos.board, c);
+        if (vectorIndexOf(run_pos_sum, c_pos) == -1) {
+            stop_delete.emplace_back(x);
+            new_pieces.stock[p].emplace_back(c);
+        }
+    }
+    while (stop_delete.size()) {
+        new_pieces.stop[p].erase(new_pieces.stop[p].begin() +
+                                 stop_delete.back());
+        stop_delete.pop_back();
+    }
+}
+
+void makeDead(size_t p, Pieces &new_pieces, const Position &pos)
+{
+    vector<int8_t> stock_delete;
+    for (size_t x = 0; x < new_pieces.stock[p].size(); x++) {
+        int c = new_pieces.stock[p][x];
+        size_t c_pos = vectorIndexOf(pos.board, c);
+        // if in other player
+        if ((c_pos & 1) == p) {
+            stock_delete.emplace_back(x);
+            new_pieces.dead[p].emplace_back(c);
+        }
+        // if multiple stock
+        else {
+            int ms = c_pos + 1;
+            int ms_pos = vectorIndexOf(pos.board, ms);
+            int ts = ms_pos + 1;
+            int ts_pos = vectorIndexOf(pos.board, ts);
+            if (vectorIndexOf(new_pieces.stock[p], ms) > -1 &&
+                (ms_pos + 1 == c ||
+                 (vectorIndexOf(new_pieces.stock[p], ts) > -1 &&
+                  ts_pos + 1 == c))) {
+                stock_delete.emplace_back(x);
+                new_pieces.dead[p].emplace_back(c);
+            }
+        }
+    }
+    while (stock_delete.size()) {
+        new_pieces.stock[p].erase(new_pieces.stock[p].begin() +
+                                  stock_delete.back());
+        stock_delete.pop_back();
+    }
+}
+
 Pieces piecesValue(const Position &pos)
 {
     Pieces new_pieces;
@@ -169,69 +237,17 @@ Pieces piecesValue(const Position &pos)
         }
     }
     // free
-    for (size_t p = 0; p < 2; p++) {
-        vector<int8_t> makefree = vectorMerge(new_pieces.hand[p],
-                                              new_pieces.free[p]);
-        for (long x = 0; x < new_pieces.stop[p].size(); x++) {
-            int stopx = new_pieces.stop[p][x];
-            int8_t stop_pos = vectorIndexOf(pos.board, stopx);
-            if (vectorIndexOf(makefree, stop_pos + 1) > -1) {
-                new_pieces.free[p].emplace_back(stopx);
-                new_pieces.stop[p].erase(new_pieces.stop[p].begin() + x);
-                makefree = vectorMerge(new_pieces.hand[p], new_pieces.free[p]);
-                x = -1;
-            }
-        }
-    }
+    makeFree(0, new_pieces, pos);
+    makeFree(1, new_pieces, pos);
+
     // stock
-    for (size_t p = 0; p < 2; p++) {
-        vector<int8_t> stop_delete;
-        for (size_t x = 0; x < new_pieces.stop[p].size(); x++) {
-            int c = new_pieces.stop[p][x];
-            int c_pos = vectorIndexOf(pos.board, c);
-            if (vectorIndexOf(run_pos_sum, c_pos) == -1) {
-                stop_delete.emplace_back(x);
-                new_pieces.stock[p].emplace_back(c);
-            }
-        }
-        while (stop_delete.size()) {
-            new_pieces.stop[p].erase(new_pieces.stop[p].begin() +
-                                     stop_delete.back());
-            stop_delete.pop_back();
-        }
-    }
+    makeStock(0, new_pieces, pos, run_pos_sum);
+    makeStock(1, new_pieces, pos, run_pos_sum);
+
     // dead
-    for (size_t p = 0; p < 2; p++) {
-        vector<int8_t> stock_delete;
-        for (size_t x = 0; x < new_pieces.stock[p].size(); x++) {
-            int c = new_pieces.stock[p][x];
-            size_t c_pos = vectorIndexOf(pos.board, c);
-            // if in other player
-            if ((c_pos & 1) == p) {
-                stock_delete.emplace_back(x);
-                new_pieces.dead[p].emplace_back(c);
-            }
-            // if multiple stock
-            else {
-                int ms = c_pos + 1;
-                int ms_pos = vectorIndexOf(pos.board, ms);
-                int ts = ms_pos + 1;
-                int ts_pos = vectorIndexOf(pos.board, ts);
-                if (vectorIndexOf(new_pieces.stock[p], ms) > -1 &&
-                    (ms_pos + 1 == c ||
-                     (vectorIndexOf(new_pieces.stock[p], ts) > -1 &&
-                      ts_pos + 1 == c))) {
-                    stock_delete.emplace_back(x);
-                    new_pieces.dead[p].emplace_back(c);
-                }
-            }
-        }
-        while (stock_delete.size()) {
-            new_pieces.stock[p].erase(new_pieces.stock[p].begin() +
-                                      stock_delete.back());
-            stock_delete.pop_back();
-        }
-    }
+    makeDead(0, new_pieces, pos);
+    makeDead(1, new_pieces, pos);
+
     return new_pieces;
 }
 
